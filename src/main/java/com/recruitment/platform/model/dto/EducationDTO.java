ff1b222738907ebd7ca8a77beb23105fd4f746e3
msedky <m.hosamsedky@hotmail.com>
2026-09-11 17:16:27 +0300

    removed no longer used s3

--- src/main/java/com/recruitment/platform/model/dto/EducationDTO.java
+++ src/main/java/com/recruitment/platform/model/dto/EducationDTO.java
@@ -8,7 +8,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import software.amazon.awssdk.services.s3.endpoints.internal.Value;
 
 import java.time.LocalDate;
 
